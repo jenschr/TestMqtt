39a0fdd5d5c839f269d191be2f489050d3e780b1
jenschr <jenschr@gmail.com>
2023-01-02 21:13:26 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+// This example uses an ESP32 Development Board
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://www.shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <WiFi.h>
+#include <MQTT.h>
+
+const char ssid[] = "Student";
+const char pass[] = "Kristiania1914";
+const char mqtt_username[] = "YourUserName";
+const char mqtt_password[] = "YourPassword";
+const char mqtt_server[]   = "mqtt.toytronics.com";
+String yourPersonalTopic;
+
+WiFiClient networkClient;
+MQTTClient mqttClient;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  String clientId = "ESP8266Client-"; // Create a random client ID
+    clientId += String(random(0xffff), HEX);
+
+  Serial.print("\nConnecting to Wifi...");
+  while (!mqttClient.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  yourPersonalTopic = "students/"; // Create a topic path based on your username
+  yourPersonalTopic += String(mqtt_username);
+  Serial.print("\nConnected to Wifi! Setting up Subscription to the topic: ");
+  Serial.println( yourPersonalTopic );
+
+  mqttClient.subscribe( yourPersonalTopic.c_str() );
+}
+
+// Note: Do not publish, subscribe or unsubscribe in this method
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming message: " + topic + " - " + payload);
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+
+  WiFi.begin(ssid, pass);
+
+  mqttClient.begin(mqtt_server, networkClient);
+  mqttClient.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  mqttClient.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!mqttClient.connected()) {
+    connect();
+  }
+
+  // publish a message every 5 second.
+  if (millis() - lastMillis > 5000) {
+    lastMillis = millis();
+    mqttClient.publish(yourPersonalTopic.c_str(), "hello");
+  }
+}
